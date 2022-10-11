a3bb3f3f218276c9befc94642307d17c3775925a
Hugo Calimache <kalim1205@gmail.com>
2022-10-11 18:11:14 +0200

    Mise à jour

--- feu04.js
+++ feu04.js
@@ -8,9 +8,32 @@ function plusGrandCarre(plateau) {
     let plateauTXT = fs.readFileSync(plateau, 'utf-8').split('\n');
     plateauTXT.pop();
 
-    console.log(plateauTXT)
+    for (let line = 0; line < plateauTXT.length; line++) {
+        plateauTXT[line] = plateauTXT[line].split('');
+    }
+
+    let maxLine = 0;
+    let l = 0;
+    let c = 0;
+    let maxColumn = 0;
+
+    for (let line = 0; line < plateauTXT.length - 1; line++) {
+        for (let column = 0; column < plateauTXT[line].length - 1; column++) {
+            if (plateauTXT[line][column] !== 'x') {
+                                
+            }
+                
+                
+        }
+    }
+
+    for (let line = 0; line < plateauTXT.length; line++) {
+        plateauTXT[line] = plateauTXT[line].join('');
+    }
+
+    console.log(plateauTXT.join('\n'));
 
 
 }
 
-console.log(plusGrandCarre(process.argv[2]))
+return plusGrandCarre(process.argv[2]);
